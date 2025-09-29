4a35d616cf8a8f842474459ff46edb2349364bd7
Matheus Goetz <goetz.dev@gmail.com>
2025-09-29 18:37:19 -0300

    style: ajustando função para criar usuário e criptografando a senha

--- src/controller/users-controller.ts
+++ src/controller/users-controller.ts
@@ -1,8 +1,20 @@
 import { Request, Response } from "express";
+import { hash } from "bcrypt" 
+import { z } from "zod"
 
 class UsersController{
-  create(req: Request, res: Response){
-    return res.status(201).json({message: "Rota users OK..."})
+  async create(req: Request, res: Response){
+    const bodySchema = z.object({
+      name: z.string().trim().min(3),
+      email: z.string().email(),
+      password: z.string().min(6)
+    })
+
+    const {name, email, password} = bodySchema.parse(req.body)
+
+    const hashedPassword = await hash(password, 8)
+
+    return res.status(201).json({message: "Usuário cadastrado!", hashedPassword})
   }
 }
 
