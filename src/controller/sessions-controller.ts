1606bf716517ff093614f482d09568068a5af605
Matheus Goetz <goetz.dev@gmail.com>
2025-10-01 20:07:53 -0300

    fixed: recuperado usuário senha e validando email e senha

--- src/controller/sessions-controller.ts
+++ src/controller/sessions-controller.ts
@@ -1,7 +1,30 @@
 import { Request, Response } from "express";
+import { AppError } from "@/utils/AppError";
+import { prisma } from "@/database/prisma";
+import { compare } from "bcrypt";
+import { z } from "zod"
 
 class SessionsController {
-  create(req: Request, res: Response){
+  async create(req: Request, res: Response){
+    const bodySchema = z.object({
+      email: z.string().email(),
+      password: z.string().min(6)
+    })
+
+    const {email, password} = bodySchema.parse(req.body)
+
+    const user = await prisma.user.findFirst({where: {email}})
+
+    if(!user){
+      throw new AppError("Email ou senha inválido", 401)
+    }
+
+    const passwordMatched = await compare(password, user.password)
+
+    if(!passwordMatched){
+      throw new AppError("Email ou senha inválido", 401)
+    }
+
     return res.json({message: "Ok!"})
   }
 }
