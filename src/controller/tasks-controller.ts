9bae9e30607ba1cf427ea3476b8c68c11e6b7236
Matheus Goetz <goetz.dev@gmail.com>
2025-10-15 19:53:03 -0300

    feat: criado função para atualizar tasks

--- src/controller/tasks-controller.ts
+++ src/controller/tasks-controller.ts
@@ -44,6 +44,47 @@ class TasksController {
 
     return res.json(task)
   }
+
+  async update(req: Request, res: Response){
+    const paramsSchema = z.object({
+      id: z.string().uuid()
+    })
+
+    const bodySchema = z.object({
+      title: z.string().trim().min(4),
+      description: z.string().trim().min(6),
+      status: z.enum([StatusTasks.pending, StatusTasks.in_progress, StatusTasks.completed]).default(StatusTasks.pending),
+      priority: z.enum([PriorityTasks.low, PriorityTasks.medium, PriorityTasks.high]).default(PriorityTasks.medium),
+      assignedTo: z.string().uuid(),
+      teamId: z.string().uuid()
+    })
+
+    const { id } = paramsSchema.parse(req.params)
+    const { title, description, status, priority, assignedTo, teamId } = bodySchema.parse(req.body)
+
+    await prisma.tasks.update({
+      data: {
+        title,
+        description,
+        status,
+        priority,
+        assignedTo,
+        teamId
+      },
+      where: {
+        id
+      }
+    })
+
+
+    return res.json()
+  }
+
+  async index(req: Request, res: Response){
+    const tasks = await prisma.tasks.findMany()
+
+    return res.json(tasks)
+  }
 }
 
 export { TasksController }
--- src/routes/tasks-routes.ts
+++ src/routes/tasks-routes.ts
@@ -4,6 +4,8 @@ import { TasksController } from "@/controller/tasks-controller";
 const tasksRoutes = Router()
 const tasksController = new TasksController()
 
+tasksRoutes.get("/", tasksController.index)
 tasksRoutes.post("/", tasksController.create)
+tasksRoutes.put("/:id", tasksController.update)
 
 export { tasksRoutes }
